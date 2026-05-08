a09c48e2362771c802cee3bd95e7c238f3bb3edf
Shawn <kakhkhorov97@gmail.com>
2026-05-08 22:48:18 +0900

    feat: Leaning String

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("================")
+print("======= Numbers =========")
 
 # In JAVA, variable is a name storage location
 # In Python, vaiable is named reference!
@@ -12,3 +12,24 @@ result1 = count.bit_count()  # method
 
 result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("======= string =========")
+
+# METHODS upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+
+result = type(course)
+print(f"the type of course(1): {result}")
+
+result = course.title()
+print(f"the result(2): {result}")
+
+result = course.upper()
+print(f"the result(3): {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result(4): {result}")
+
+print("======= boolean =========")
