1870d7952148d79da75d1490a775dd0efd0a0eaf
Shawn <kakhkhorov97@gmail.com>
2026-05-12 15:08:00 +0900

    feat: Learning python objects

---
+++ objects.py
@@ -0,0 +1,26 @@
+''' Objests
+(1) What is object
+(2) Iterible objects & RANGE
+(3) DICTIONARY
+(4) Error handling stystem
+'''
+
+import array  # package/module
+import math
+print("===== What is object =====")
+# An object has state and method properties
+# Everything is an object in Python!
+
+print(type('Hello world!'))
+print(type(100))
+print(type(True))
+print(type(array))
+
+# Paradigm > Functional programming & OOP
+# OOP 4 Concepts > Abstraction | Ecncapsulation | Inheritance | Polimorphism
+
+result1 = math.ceil(92.8)  # CALL
+print("result1:", result1)
+
+result2 = math.ceil(99.9)
+print("result2:", result2)
